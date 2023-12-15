684183b05ba6a9072bebd1aee26769bb681b0f73
danilo-leite <danilo.cebolino@gmail.com>
2023-12-15 20:30:42 -0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="h-52 flex items-center justify-center bg-slate-700 text-teal-200">
+      <p>
+        <span className=" text-teal-300/100">React + TS Todo</span> @ 2021
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-700 text-center p-4">
+      <h1 className="text-teal-200/100 text-2xl">React + TS Todo</h1>
+    </header>
+  );
+};
+
+export default Header;
